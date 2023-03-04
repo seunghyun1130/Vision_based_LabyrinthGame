3044df32b4bde5b7a1234a8534de4395f1a7a16e
Seoyeon Choi <97090402+n00Nspr1ng@users.noreply.github.com>
2023-03-04 21:58:54 +0900

    Add files via upload

---
+++ RL_pretraining/env/maze_generator.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+
+
+# from mazelib import Maze
+# from mazelib.generate.Prims import Prims
+# from mazelib.solve.BacktrackingSolver import BacktrackingSolver
+
+
+# class MazeGenerator:
+#     def __init__(self):
+#         ''' '''
+
+#     def maze(self):
+    
+#         m = Maze()
+#         m.generator = Prims(4, 4)
+#         m.generate()
+
+
+#         m.solver = BacktrackingSolver()
+#         m.generate_entrances(start_outer=False, end_outer=False)
+#         m.solve()
+
+#         print(m)
+
+#         print(m.grid)
+
+#     #def save(self, dir):
+
+
+
+
+
+# if __name__=="__main__":
+#     maze = MazeGenerator()
+#     maze.maze()
+    
